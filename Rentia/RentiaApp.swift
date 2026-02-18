bb6c7c458dfb035f2a9a74a296b1085ed76e1aa0
Jose carlos Rodriguez <jcrodriguez@opentrends.net>
2026-02-18 16:15:35 +0100

    Initial Commit

---
+++ Rentia/RentiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentiaApp.swift
+//  Rentia
+//
+//  Created by Spiderman on 18/2/26.
+//
+
+import SwiftUI
+
+@main
+struct RentiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
